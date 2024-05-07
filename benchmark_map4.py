359d536ff71b12a29d837cd048f3438ac4ffa811
Francois Berenger <unixjunkie@sdf.org>
2024-05-07 10:11:02 +0900

    benchmark_map4.py: RFR instead of XGBoost

--- benchmark_map4.py
+++ benchmark_map4.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 # In[7]:
@@ -30,10 +30,10 @@ def fp_as_array(mol):
     return arr
 
 
-# Build a dataframe from a SMILES file. 
+# Build a dataframe from a SMILES file.
 # 1. Read the SMILES
 # 2. Calculate a MAP4 fingerprint and put that into the dataframe
-# 3. Calculate a Morgan fingerprint and put thta into the dataframe 
+# 3. Calculate a Morgan fingerprint and put thta into the dataframe
 
 # In[3]:
 
@@ -51,34 +51,69 @@ def build_dataframe(input_file_name):
 # Benchmark a dataset.
 # 1. Split into training and test sets using the sklearn defaults
 # 2. Train and test an XGBoost regressor with MAP4 fingerprints and record the R<sup>2</sup>
-# 3. Train and test an XGBoost regressor with Morgan fingerprints and record the R<sup>2</sup>  
-# 
+# 3. Train and test an XGBoost regressor with Morgan fingerprints and record the R<sup>2</sup>
+#
 # Return lists of R<sup>2</sup> for MAP4 and Morgan
 
 # In[41]:
 
 
-def benchmark_dataset(df,name,cv_folds=10):
+def benchmark_dataset(df, name, cv_folds=5):
     map4_list = []
     morgan_list = []
-    for i in tnrange(cv_folds,desc=name):
+    for i in tnrange(cv_folds, desc=name):
         train, test = train_test_split(df)
-        map4_xgb = XGBRegressor()
-        map4_xgb.fit(np.array([x for x in train.map4.values]),train.IC50)
-        map4_pred = map4_xgb.predict(np.array([x for x in test.map4.values]))        
-        map4_r2 = r2_score(test.IC50,map4_pred)  
+
+        # # XGB
+        # map4_xgb = XGBRegressor()
+        # X_train = np.array([x for x in train.map4.values])
+        # y_train = train.IC50
+        # map4_xgb.fit(X_train, y_train)
+        # map4_pred = map4_xgb.predict(np.array([x for x in test.map4.values]))
+        # map4_r2 = r2_score(test.IC50, map4_pred)
+        # map4_list.append(map4_r2)
+
+        # morgan_xgb = XGBRegressor()
+        # morgan_xgb.fit(np.array([x for x in train.morgan.values]), train.IC50)
+        # morgan_pred = morgan_xgb.predict(np.array([x for x in test.morgan.values]))
+        # morgan_r2 = r2_score(test.IC50, morgan_pred)
+        # morgan_list.append(morgan_r2)
+
+        X_train = np.array([x for x in train.map4.values])
+        y_train = train.IC50
+
+        X_test = np.array([x for x in test.map4.values])
+        y_test = test.IC50
+
+        # RFR
+        map4_rfr = RandomForestRegressor(n_estimators = 100,
+                                         criterion = 'squared_error',
+                                         #n_jobs = nprocs, # FBR: check if improves perfs
+                                         oob_score = True,
+                                         min_samples_leaf = 1,
+                                         max_features = max_features, # FBR: fraction; could be optimized
+                                         max_samples = None) # sklearn default
+        map4_rfr.fit(X_train, y_train)
+        map4_pred = map4_rfr.predict(X_test)
+        map4_r2 = r2_score(y_test, map4_pred)
         map4_list.append(map4_r2)
-        
-        morgan_xgb = XGBRegressor()
-        morgan_xgb.fit(np.array([x for x in train.morgan.values]),train.IC50)
-        morgan_pred = morgan_xgb.predict(np.array([x for x in test.morgan.values]))
-        morgan_r2 = r2_score(test.IC50,morgan_pred)
+
+        morgan_rfr = RandomForestRegressor(n_estimators = 100,
+                                           criterion = 'squared_error',
+                                           #n_jobs = nprocs, # FBR: check if improves perfs
+                                           oob_score = True,
+                                           min_samples_leaf = 1,
+                                           max_features = max_features, # FBR: fraction; could be optimized
+                                           max_samples = None) # sklearn default
+        morgan_rfr.fit(X_train, y_train)
+        morgan_pred = morgan_rfr.predict(X_test)
+        morgan_r2 = r2_score(y_test, morgan_pred)
         morgan_list.append(morgan_r2)
-        
+
     return np.array(map4_list), np.array(morgan_list)
 
 
-# Run the bechmark on a set of files.  
+# Run the bechmark on a set of files.
 
 # In[132]:
 
@@ -94,7 +129,7 @@ def run_benchmarks(file_spec):
     return result_list
 
 
-# Run the benchmark, on my MacBook Pro this took 21 minutes. 
+# Run the benchmark, on my MacBook Pro this took 21 minutes.
 
 # In[ ]:
 
@@ -102,7 +137,7 @@ def run_benchmarks(file_spec):
 get_ipython().run_line_magic('time', 'result_list = run_benchmarks("*.smi")')
 
 
-# Put the results into a dataframe and write the dataframe to disk. 
+# Put the results into a dataframe and write the dataframe to disk.
 
 # In[141]:
 
@@ -125,7 +160,7 @@ result_df.to_csv("map4_morgan_comparison.csv",index=False)
 result_df.head()
 
 
-# Convert the dataframe to a format appropriate for a boxplot. The melt function combines the map4 and morgan columns into one column. 
+# Convert the dataframe to a format appropriate for a boxplot. The melt function combines the map4 and morgan columns into one column.
 
 # In[10]:
 
@@ -147,7 +182,7 @@ ax.set(ylabel="Dataset",xlabel="$R^2$")
 _ = ax.legend(loc='upper right', bbox_to_anchor=(1.2, 1.0), ncol=1)
 
 
-# Calculate [Cohen's d](https://machinelearningmastery.com/effect-size-measures-in-python/) for the comparisons. 
+# Calculate [Cohen's d](https://machinelearningmastery.com/effect-size-measures-in-python/) for the comparisons.
 
 # In[12]:
 
@@ -159,7 +194,7 @@ for k,v in result_df.groupby("dataset"):
 cohen_df = pd.DataFrame(cohen_list,columns=["dataset","cohen_d"])
 
 
-# Make a plot of the effect is for each of the datasets. Lines are drawn at 
+# Make a plot of the effect is for each of the datasets. Lines are drawn at
 # * Small Effect Size: d=0.20
 # * Medium Effect Size: d=0.50
 # * Large Effect Size: d=0.80
@@ -182,7 +217,3 @@ cohen_df.query("cohen_d < -0.8").shape
 
 
 # In[ ]:
-
-
-
-
