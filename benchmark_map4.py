d5477c6442dd268951bd4e265ebb131706f62567
Francois Berenger <unixjunkie@sdf.org>
2024-05-07 14:04:51 +0900

    benchmark_map4.py: get rid of the plots

--- benchmark_map4.py
+++ benchmark_map4.py
@@ -110,7 +110,7 @@ def benchmark_dataset(df, name, cv_folds=5):
         morgan_r2 = r2_score(y_test, morgan_pred)
         morgan_list.append(morgan_r2)
 
-    return np.array(map4_list), np.array(morgan_list)
+    return (np.array(map4_list), np.array(morgan_list))
 
 
 # Run the bechmark on a set of files.
@@ -144,76 +144,3 @@ get_ipython().run_line_magic('time', 'result_list = run_benchmarks("*.smi")')
 
 result_df = pd.DataFrame(result_list,columns=["dataset","map4","morgan"])
 result_df.to_csv("map4_morgan_comparison.csv",index=False)
-
-
-# In case we come back to this later, uncomment the line below and start from there.
-
-# In[1]:
-
-
-#result_df = pd.read_csv("map4_morgan_comparison.csv")
-
-
-# In[9]:
-
-
-result_df.head()
-
-
-# Convert the dataframe to a format appropriate for a boxplot. The melt function combines the map4 and morgan columns into one column.
-
-# In[10]:
-
-
-melt_df = result_df.melt(id_vars="dataset")
-melt_df.columns = ["dataset","method","r2"]
-melt_df.head()
-
-
-# Make a boxplot of the results.
-
-# In[11]:
-
-
-sns.set(rc={'figure.figsize': (15, 12)})
-sns.set(font_scale=1.5)
-ax = sns.boxplot(y="dataset",x="r2",hue="method",orient="h",data=melt_df,order=sorted(melt_df.dataset.unique()))
-ax.set(ylabel="Dataset",xlabel="$R^2$")
-_ = ax.legend(loc='upper right', bbox_to_anchor=(1.2, 1.0), ncol=1)
-
-
-# Calculate [Cohen's d](https://machinelearningmastery.com/effect-size-measures-in-python/) for the comparisons.
-
-# In[12]:
-
-
-cohen_list = []
-for k,v in result_df.groupby("dataset"):
-    delta = v.map4 - v.morgan
-    cohen_list.append([k,np.mean(delta)/np.std(delta)])
-cohen_df = pd.DataFrame(cohen_list,columns=["dataset","cohen_d"])
-
-
-# Make a plot of the effect is for each of the datasets. Lines are drawn at
-# * Small Effect Size: d=0.20
-# * Medium Effect Size: d=0.50
-# * Large Effect Size: d=0.80
-
-# In[13]:
-
-
-ax = sns.barplot(y="dataset",x="cohen_d",data=cohen_df,orient="h")
-ax.set(ylabel="Dataset",xlabel="Cohen's d")
-_ = ax.set(xlim=[-5,5])
-ax.vlines(0,0,cohen_df.shape[0],linestyles="solid")
-for i in [-0.8,-0.5,-0.2,0.2,0.5,0.8]:
-    ax.vlines(i,0,cohen_df.shape[0],linestyles="dotted")
-
-
-# In[14]:
-
-
-cohen_df.query("cohen_d < -0.8").shape
-
-
-# In[ ]:
